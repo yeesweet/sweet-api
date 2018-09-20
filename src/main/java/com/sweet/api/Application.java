80683592b2d3024d4b7e8ff993c838fc3a3c465e
zhang.hp <zhang.hp@belle.com.cn>
2018-09-20 11:35:29 +0800

    修改基本配置

--- src/main/java/com/sweet/api/Application.java
+++ src/main/java/com/sweet/api/Application.java
@@ -2,27 +2,22 @@ package com.sweet.api;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
-@EnableWebMvc
 @EnableTransactionManagement
 @ComponentScan(basePackages = "com.sweet.api")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     protected final static Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setBannerMode(Banner.Mode.OFF);
-        app.run(args);
+        new SpringApplication(Application.class).run(args);
         logger.info("PortalApplication is success!");
     }
-
 }
