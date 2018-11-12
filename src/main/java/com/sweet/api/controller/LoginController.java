7e3105aec55eb117e21d9bb5a81325fa5c111c34
wang.s2 <wang.s2@belle.com.cn>
2018-11-12 17:56:35 +0800

    登录

--- src/main/java/com/sweet/api/controller/LoginController.java
+++ src/main/java/com/sweet/api/controller/LoginController.java
@@ -4,8 +4,6 @@ import com.sweet.api.commons.ResponseMessage;
 import com.sweet.api.entity.req.LoginReq;
 import com.sweet.api.entity.res.LoginResp;
 import com.sweet.api.service.ILoginService;
-import com.sweet.api.wx.entity.AccessToken;
-import com.sweet.api.wx.service.WechatCommon;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("login")
 @Slf4j
 public class LoginController {
 
